e6aec017a0badf73259ee106e34a7893ce18cc1a
tseley <tseley0@gmail.com>
2025-03-01 21:27:32 +0100

    second commit

--- dump.sql
+++ dump.sql
@@ -15,10 +15,10 @@ CREATE TABLE IF NOT EXISTS etudiants (
 
 
 INSERT INTO etudiants (prenom, nom, email, cv, dt_naissance, isAdmin, dt_mis_a_jour) VALUES
-('Marie', 'Dupont', 'marie.dupont@email.com', 'CV de Marie Dupont avec expérience en marketing', '1998-05-15', false, NOW()),
-('Thomas', 'Martin', 'thomas.martin@email.com', 'CV de Thomas Martin, spécialiste en développement web', '1995-11-23', true, NOW()),
-('Sophie', 'Bernard', 'sophie.bernard@email.com', 'CV de Sophie Bernard, passionnée de design', '1999-03-10', false, NOW()),
-('Lucas', 'Petit', 'lucas.petit@email.com', 'CV de Lucas Petit avec compétences en réseaux', '1997-07-08', false, NOW()),
+('Marie', 'Dupont', 'marie.dupont@email.com', 'CV de Marie Dupont avec expérience en marketing', '1998-05-15', false),
+('Thomas', 'Martin', 'thomas.martin@email.com', 'CV de Thomas Martin, spécialiste en développement web', '1995-11-23', true),
+('Sophie', 'Bernard', 'sophie.bernard@email.com', 'CV de Sophie Bernard, passionnée de design', '1999-03-10', false, ),
+('Lucas', 'Petit', 'lucas.petit@email.com', 'CV de Lucas Petit avec compétences en réseaux', '1997-07-08', false),
 ('Emma', 'Leroy', 'emma.leroy@email.com', 'CV d\'Emma Leroy, experte en cybersécurité', '1996-01-30', false),
 ('Hugo', 'Moreau', 'hugo.moreau@email.com', 'CV de Hugo Moreau, développeur mobile', '1994-09-12', false),
 ('Chloé', 'Dubois', 'chloe.dubois@email.com', 'CV de Chloé Dubois, data scientist', '1998-12-05', false),
